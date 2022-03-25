49d4bb97c92f19c5cf20b7943bc8c486f97bb1b4
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-03-25 18:29:24 +0100

    add contact,

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { uuid } from "uuidv4";
+import AddContact from "./AddContact";
 import "./App.css";
-
+import ContactList from "./ContactList";
+import Header from "./Header";
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    console.log(contacts);
+    setContacts([...contacts, { id: uuid(), ...contacts }]);
+  };
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
   return (
-    <div className="App">
-      <h1>God is good to me and my family</h1>
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
---
+++ src/components/ContactCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import lekan from "../images/lekan.jpg";
+
+const ContactCard = (props) => {
+  const { id, name, email } = props.contacts;
+  return (
+    <div className="item">
+      <img className="ui avatar image" alt="" src={lekan} />
+      <div className="content">
+        <div className="Header">{name}</div>
+        <div>{email}</div>
+      </div>
+      <i
+        className="trash alternate outline icon"
+        style={{ color: "red", marginTop: "10px" }}
+      ></i>
+    </div>
+  );
+};
+export default ContactCard;
