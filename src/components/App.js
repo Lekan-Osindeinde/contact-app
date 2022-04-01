51a7f887d54fdefa6b83beb941bda5025e898850
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-04-01 16:23:03 +0100

    uuid and delete function

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,15 @@ import "./App.css";
 import ContactList from "./ContactList";
 import Header from "./Header";
 
+// const { uuid } = require("uuidv4");
+
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
     console.log(contacts);
-    setContacts([...contacts, contact]);
+    // console.log(uuid());
+    // setContacts([...contacts, { id: uuid(), ...contact }]);
   };
   useEffect(() => {
     const retrieveContacts = JSON.parse(
